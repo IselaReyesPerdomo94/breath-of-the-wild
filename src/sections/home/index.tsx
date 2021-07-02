07e66c5357a8f03983b5f6e97acb8614ab935408
IselaReyesPerdomo94 <reyesp.isela@gmail.com>
2021-07-01 22:30:40 -0700

    Adding heroe page

---
+++ src/sections/home/index.tsx
@@ -0,0 +1,11 @@
+import Heroe from '../Heroe';
+
+const Home = () => {
+  return(<header>
+    <h1>All about Breath of the wild</h1>
+    <p>This site has the porpuse to show all the items in Zelda: Breath of the wild and practice my skills</p>
+    <Heroe />
+  </header>);
+};
+
+export default Home;
