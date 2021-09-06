3b0650fa64ed456e9cd7e36a59cb95b525092453
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2021-09-05 22:00:40 -0700

    Creating grid for cards in creatures of breath of the wild

--- src/sections/Creatures/Creatures.tsx
+++ src/sections/Creatures/Creatures.tsx
@@ -1,14 +1,36 @@
 import { useCreatures } from './hooks';
+import './styles.css';
 
 const Creatures = () => {
   const { data: response, isLoading } = useCreatures();
 
   const foodCreatures = response?.data?.food;
 
-  return(<section>
-    <h2>Creatures</h2>
+  return (<section className="creatures-section">
+    <h2 className="title">Creatures</h2>
     {isLoading && (<p>Is loading, hold on </p>)}
-    {foodCreatures && foodCreatures.map(creature => (<p key={creature.id}>{creature.name}</p>))}
+    <div className="creatures-container">
+    {foodCreatures && foodCreatures.map(creature => (
+      <article key={creature.id} className="card-wrapper">
+        <h6>
+          {creature.name}
+        </h6>
+        <img src={creature.image} alt={creature.name} />
+        <p>
+          <span>ID: {creature.id}</span>
+          <span>Hearts recovered: {creature.hearts_recovered}</span>
+        </p>
+        <p>Description: {creature.description}</p>
+        <p>Cooking effect: {creature.cooking_effect}</p>
+        <p>Where to find them?</p>
+          <ul>
+            {creature.common_locations.map((location, index) => (
+              <li key={`${index}_${location}`}>{location}</li>)
+            )}
+          </ul>
+      </article>)
+    )}
+    </div>
   </section>);
 };
 
